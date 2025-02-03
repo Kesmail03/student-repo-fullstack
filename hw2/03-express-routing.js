159dfddf35210510bdcfd5fc87cd78b752bd0d7f
Khaled Esmail <kesmail@pdx.edu>
2025-02-03 02:44:56 -0800

    HW3

--- hw2/03-express-routing.js
+++ hw2/03-express-routing.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -13,21 +13,56 @@ const port = process.env.PORT || 5001;
 // For other routes, such as http://localhost:5001/other, this exercise should return a status code 404 with '404 - page not found' in html format
 
 const routes = [
-  'welcome',
-  'redirect',
-  'redirected',
-  'cache',
-  'cookie',
-  'other',
+  "welcome",
+  "redirect",
+  "redirected",
+  "cache",
+  "cookie",
+  "other",
 ];
 
-app.get('/', (req, res) => {
+// Add your code here
+app.get("/", (req, res) => {
   res.status(200);
-  res.set({ 'Content-Type': 'text/html' });
-  res.send('Express Routing Exercise');
+  res.set({ "Content-Type": "text/html" });
+  res.send("Express Routing Exercise");
 });
 
-// Add your code here
+app.get("/welcome", (req, res) => {
+  res.status(200);
+  res.set({ "Content-Type": "text/html" });
+  res.send("Welcome to the page");
+});
+
+app.get("/redirect", (req, res) => {
+  res.redirect(302, "/redirected");
+});
+
+app.get("/redirected", (req, res) => {
+  res.status(200).send("<h1>You have been redirected!</h1>");
+});
+
+app.get("/cache", (req, res) => {
+  res.status(200);
+  res.set("cache-control", "public, max-age-86400");
+  res.send("this resource was cached");
+});
+
+app.get("/cookie", (req, res) => {
+  res.status(200);
+  res.cookie("hello", "world");
+  res.send("cookies... yummm");
+});
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.cookie("hello", "world");
+  res.send("cookies... yummm");
+});
+
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
