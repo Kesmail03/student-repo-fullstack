2364787cae108c6d6e60166e4f1a08786e50ac90
Khaled Esmail <kesmail@pdx.edu>
2025-02-03 02:44:56 -0800

    HW3

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,7 +1,45 @@
 /** Exercise 01 - Coins **/
 
-// Add your function here 
+// Add your function here
 
+calculateChange = (change) => {
+  let dollar = 0;
+  let quarter = 0;
+  let dime = 0;
+  let nickel = 0;
+  let penny = 0;
+  change = Math.round(change * 100);
+  if (change > 10000) {
+    return "Error: value is too large";
+  } else {
+    while (change >= 1) {
+      if (change >= 100) {
+        change -= 100;
+        dollar += 1;
+      } else if (change >= 25) {
+        change -= 25;
+        quarter += 1;
+      } else if (change >= 10) {
+        change -= 10;
+        dime += 1;
+      } else if (change >= 5) {
+        change -= 5;
+        nickel += 1;
+      } else {
+        change -= 1;
+        penny += 1;
+      }
+    }
+    let result = [];
+    if (dollar > 0) result.push(`${dollar} dollar${dollar > 1 ? "s" : ""}`);
+    if (quarter > 0) result.push(`${quarter} quarter${quarter > 1 ? "s" : ""}`);
+    if (dime > 0) result.push(`${dime} dime${dime > 1 ? "s" : ""}`);
+    if (nickel > 0) result.push(`${nickel} nickel${nickel > 1 ? "s" : ""}`);
+    if (penny > 0) result.push(`${penny} penn${penny === 1 ? "y" : "ies"}`);
+
+    return result.join(", ") || "No change needed";
+  }
+};
 // Sample test cases
 console.log(calculateChange(4.62));
 // $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
@@ -10,4 +48,4 @@ console.log(calculateChange(0.16));
 console.log(calculateChange(150.11));
 // $150.11 ==> Error: the number is too large
 
-// Add additional test cases here 
+// Add additional test cases here
--- hw2/04-form.js
+++ hw2/04-form.js
@@ -1,5 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 // http://localhost:5001/form should return a form with input elements for username, email, and submit button
 
 // http://localhost:5001/submit should return all the data the user entered
+
+app.get("/form", (req, res) => {
+  res.send(
+    `<form action="/submit" method="POST">
+        <label for="username">Name:</label>
+        <input type="text" name="username"/>
+        <br>
+        <label for="email">Email:</label>
+        <input type="text" name="email"/>
+        <br>
+        <label for="comments">Comments:</label>
+        <input type="text" name="comments"/>
+        <br>
+        <label for="newsletter">Newsletter:</label>
+        <input type="text" name="newsletter"/>
+        <br>
+        <button type="submit">Submit</button>
+        </form>`
+  );
+});
+
+app.post("/submit", (req, res) => {
+  const { username, email, comments, newsletter } = req.body;
+  res.send(`<h3>Received Input:</h3>
+  <p>Name: ${username}</p>
+  <p>Email: ${email}</p>
+  <p>Comments: ${comments}</p>
+  <p>Newsletter: ${newsletter}</p>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
